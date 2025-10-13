89c2464349cd8959889e1f72fe02b55af0d7c15a
Beknur <ualihanulybeknur@gmail.com>
2025-10-14 04:35:39 +0500

    add the generared_data

--- apps/movies/admin.py
+++ apps/movies/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Film, Comment
 
-# Register your models here.
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+    readonly_fields = ('user', 'created_at')
+    fields = ('user', 'text', 'created_at')
+
+
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ('title', 'description')
+    list_filter = ('id',)
+    ordering = ('title',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('film', 'user', 'created_at')
+    search_fields = ('film__title', 'user__username', 'text')
+    list_filter = ('created_at', 'film')
+    ordering = ('-created_at',)
+
+    readonly_fields = ('user', 'film', 'created_at')
