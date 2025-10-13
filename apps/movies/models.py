035f74f31214cb00a563b55891553cddc31f4788
Beknur <ualihanulybeknur@gmail.com>
2025-10-14 03:46:25 +0500

    add ER diagram

--- apps/movies/models.py
+++ apps/movies/models.py
@@ -5,7 +5,7 @@ class Film(models.Model):
     title=models.CharField(max_length=255)
     description=models.TextField()
 
-    def str(self):
+    def __str__(self):
         return self.title
     
 class Comment(models.Model):
