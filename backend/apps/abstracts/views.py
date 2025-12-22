237f853c00f95f2b369cd64215a729a50d3cc576
elitekbtu <satbaldievst@mail.ru>
2025-12-22 18:38:35 +0500

    kinopisk backend 2.0.0

--- backend/apps/abstracts/views.py
+++ backend/apps/abstracts/views.py
@@ -1,20 +1,15 @@
 # Django Third-Party modules
-from rest_framework import viewsets, serializers
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.status import HTTP_200_OK
 from rest_framework.permissions import AllowAny
 
 
-class HealthSerializer(serializers.Serializer):
-    status = serializers.CharField()
-
-
 class HealthCheckViewSet(viewsets.GenericViewSet):
     """A simple ViewSet for health check endpoint."""
 
     permission_classes = [AllowAny]
-    serializer_class = HealthSerializer
 
     def health_check(self, request: Request, *args, **kwargs) -> Response:
         return Response({"status": "ok"}, status=HTTP_200_OK)
